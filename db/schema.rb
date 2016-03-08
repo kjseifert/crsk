c8e58a9b0c326ceed6f523042a5cd8cc0528de7c
Kevin Seifert <kevin.j.seifert@gmail.com>
2016-03-08 20:45:30 +0000

    added link field

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227192902) do
+ActiveRecord::Schema.define(version: 20160308202010) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.string   "link"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
